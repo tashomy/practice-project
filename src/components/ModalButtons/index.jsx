fd2732e95ee32003d7153df8f88b62c219fd4e6f
Natasa <mijatovicn650@gmail.com>
2022-05-04 15:16:56 +0200

    all data fetching works, styles need improvement

--- src/components/ModalButtons/index.jsx
+++ src/components/ModalButtons/index.jsx
@@ -4,26 +4,20 @@ const ModalButtons = (props) => {
   const getData = (e) => {
     e.preventDefault();
     const type = e.target.innerHTML.toLowerCase();
-    let id = e.target.id;
-    console.log(id);
-    if (id == 0) id = 10;
-    props.onClick(type, id);
+    let idFromUrl = `${props.url.charAt(
+      props.url.length - 3
+    )}${props.url.charAt(props.url.length - 2)}`;
+    if (idFromUrl.charAt(0) === "/") {
+      idFromUrl = idFromUrl.substring(1);
+    }
+
+    props.onClick(type, idFromUrl);
   };
   return (
     <div className={`btn-div ${props.className}`}>
       {props.content.map((item, i) => {
         return (
-          <button
-            onClick={getData}
-            key={i}
-            id={
-              props.page === 1
-                ? `${props.url.charAt(props.url.length - 2)}`
-                : `${props.url.charAt(props.url.length - 3)}${props.url.charAt(
-                    props.url.length - 2
-                  )}`
-            }
-          >
+          <button onClick={getData} key={i}>
             {item.text}
           </button>
         );
